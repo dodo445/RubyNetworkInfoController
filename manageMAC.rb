b2d7d32e877566da7abb26a4647d119da873cb8c
Doğukan <dogukanucak94@gmail.com>
2016-08-09 21:35:49 +0300

    Update manageMAC.rb

--- manageMAC.rb
+++ manageMAC.rb
@@ -6,27 +6,38 @@ require 'green_shoes'
 BEGIN {
 }
 puts "NETWORK INFO SCRIPT".red
-interfaces = `ifconfig | grep MULTICAST | awk '{print $1;}'`
-mac_address = ` ifconfig | grep Ethernet | awk '{print $2;}'`
-mac_address = mac_address.split(/\n+/)
-interfaces=interfaces.split(/\n+/)
-i = 0;
-devices =""
-puts "NETWORK INTERFACES:"
-interfaces.each do |interface|
-  devices += "#{interface} => #{mac_address[i]}\n"
-  i=i+1
-end
 
+def getInterfaces
+  interfaces = `ifconfig | grep MULTICAST | awk '{print $1;}'`
+  interfaces=interfaces.split(/\n+/)
+  return interfaces
+end
+def getMac
+  mac_address = ` ifconfig | grep Ethernet | awk '{print $2;}'`
+  mac_address = mac_address.split(/\n+/)
+  return mac_address
+end
+def displayDevices
+    i = 0;
+    devices =""
+    puts "NETWORK INTERFACES:"
+    getInterfaces.each do |interface|
+    devices += "#{interface} => #{getMac[i]}\n"
+    i=i+1
+  end
+  return devices
+end
 Shoes.app(width: 400, height: 200, resizable: false , title: "NETWORK INFO") do
   background "royalblue"
-  stack do
-    # Background, text and a button: both are elements!
-    @back  = background royalblue
-    @text  = banner (devices)
-    # And so, both can be styled.
-    @text.style size: 12, stroke:green, margin: 10, color:red
-    @back.style height: 10
-  end
 
+  @devices_text=para(displayDevices,:align => "center",:top => 30,)
+  @change_button=button( "REFRESH" ) do
+    @devices_text.replace(displayDevices)
+  end
+  Thread.new do
+    while true do
+      @devices_text.replace(displayDevices)
+      sleep 30
+    end
+  end
 end
