4b74869c7b5f6dee1f12c63c96a268dd92d9d264
dodo445 <dogukanucak94@gmail.com>
2016-08-12 22:55:53 +0300

    Added Mac reset to permanent

--- View/homepage.rb
+++ View/homepage.rb
@@ -21,37 +21,47 @@ BEGIN {
 
 
 
-Shoes.app {
+Shoes.app(width:500,title:"NetworkController") {
 
-stack(margin: 24){
+stack(margin: 24) {
     other , wifi = device_info()
-    title "Network Controller"
-    para strong("Interfaces"), " Controller"
+       para strong("Interfaces"), " Controller"
       @other_devices_text = para(other)
       para strong("Wireless"), " Controller"
       @wifi_device_text = para(wifi)
     stack do
+
     @edit_line = edit_line
     @change_button=button( "CHANGE" ) do
+
       if @edit_line.text == ""
         `ifconfig #{@wifi_device.getName()} down`
-      output= `macchanger -r #{@wifi_device.getName()}`
+      output= `macchanger -r #{@wifi_device.getName()} 2>&1`
       `ifconfig #{@wifi_device.getName()} up`
       else
-        `ifconfig #{@wifi_device.getName()} down`
-       output = `macchanger -m #{@edit_line.text()} #{@wifi_device.getName()}`
+        m1 = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/.match(@edit_line.text()).class.to_s
+
+        if (m1.include? "Match")
+          `ifconfig #{@wifi_device.getName()} down`
+       output = `macchanger -m #{@edit_line.text()} #{@wifi_device.getName()} 2>&1`
         `ifconfig #{@wifi_device.getName()} up`
-
-      end
-        unless output.include? ("ERROR")
-          alert("ERROR: NO VALID MAC IS ENTERED...")
+          @wifi_device_text.replace(wifi)
         else
-          alert(output)
+          output = "NO VALID MAC IS ENTERED"
         end
-          end
+        alert(output)
       end
+    end
+      @reset_button=button("RESET",margin_left:75,margin_top:-29) do
 
-  }
+        `ifconfig #{@wifi_device.getName()} down`
+        output= `macchanger -p #{@wifi_device.getName()} 2>&1`
+        `ifconfig #{@wifi_device.getName()} up`
+        @wifi_device_text.replace(wifi)
+        alert(output)
+      end
+      end
+}
 Thread.new do
   every 5 do
     other,wifi = device_info
@@ -62,7 +72,9 @@ end
 }
 
 
+END  {
 
+  }
 
 
 
